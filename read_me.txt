3dd5665aba78df65a3b9842e024a9c3ca6328414
Hemumkote <hemumkote@gmail.com>
2025-09-24 18:45:04 +0530

    first commit

---
+++ read_me.txt
@@ -0,0 +1,5 @@
+npm i express -> to create the express server
+
+package-lock.json -> to lock the modules or the packages version 
+
+npm i nodemon -> to restart the server automatically on changes
---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express(); // instance of an express js application
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(
+    "server is running on the port 3000 oprn in http://localhost:3000 oko"
+  );
+}); // now our app is listening thru that port ,and the Callback is called only on server running
+
+app.use("/goola", (req, res) => {
+  res.send("The app is running  ijijij ");
+});
