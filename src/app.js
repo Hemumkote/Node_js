0d82bbd6459b57f27c67d8cc33a75dc70ae5be4c
Hemumkote <hemumkote@gmail.com>
2025-11-06 22:40:29 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ app.listen(PORT, () => {
   );
 }); // now our app is listening thru that port ,and the Callback is called only on server running
 
+app.get("/", (req, res) => {
+  res.send("The app is running  ");
+});
+
 app.use("/goola", (req, res) => {
   res.send("The app is running  ijijij ");
 });
